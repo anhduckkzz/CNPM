d9bc528817f46d38ec9b3524c87cfcefa7db25e6
Trần Anh Đức <158149029+anhduckkzz@users.noreply.github.com>
2025-11-10 20:20:18 +0700

    add analytics and insights

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      <Analytics />
+      <SpeedInsights />
+    </>
   </React.StrictMode>,
 );
